5697f003ceef927dc1a6f3839bd30bd363e7971e
luzzhen <1774903902@qq.com>
2021-01-24 16:45:01 +0800

    两组件之间

---
+++ _课件/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    {{ msg }}
+	<base-son />
+  </div>
+  
+</template>
+
+<script>
+import baseSon from "./Son.vue";
+
+export default {
+  components: {
+    baseSon,
+  },
+  data() {
+    return {
+      msg: "luzzhen",
+    };
+  },
+};
+</script>
+
+<style>
+div {
+ 
+  background-color: red;
+}
+</style>
---
+++ _课件/Son.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    son
+    <p>i am son</p>
+  </div>
+</template>
+
+<script>
+export default {
+    
+}
+
+</script>
+
+<style scoped>
+div {
+ 
+  background-color: greenyellow;
+}
+</style>
